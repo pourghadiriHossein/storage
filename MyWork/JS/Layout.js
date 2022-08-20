7124763018f5ff70ca861b65e0fbe2b677eaa1b1
pourghadiriHossein <49945753+pourghadiriHossein@users.noreply.github.com>
2022-08-20 15:55:39 +0430

    Delete MyWork directory

--- MyWork/JS/Layout.js
+++
@@ -1,5 +0,0 @@
-let date = new Date();
-customizeDate = 
-" سال : "+date.getFullYear()+" ماه : "+ date.getMonth()+" روز :‌ "+date.getDate()
-+" "+" ساعت : "+date.getHours()+" دقیقه : "+date.getMinutes();
-document.getElementById('customizeDate').innerHTML = customizeDate;
